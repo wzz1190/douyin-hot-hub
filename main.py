c5d1c835383800a4ffb939ac66be3d903c72ae68
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2022-06-24 11:14:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,21 @@ def generate_readme(searches):
 
     return readme
 
+def redmd()
+    now = util.current_time()
+    files = os.path.join('raw', now)
+    g = os.walk(files)
+    
+  readme = ''
+ readmes = ''
+for path,dir_list,file_list in g:  
+    for file_name in file_list:  
+      file=(os.path.join(path, file_name) 
+            with open(file) as f:
+            readme = f.read()
+            readmes=readmes+readme
+            filee = os.path.join('archives', 'guagua.md')
+            util.write_text(filee, readmes)
 
 def save_readme(md):
     
@@ -100,6 +115,7 @@ def run():
     # 最新数据
     todayMd = generate_readme(searches)
     save_readme(todayMd)
+    redmd()
     # 归档
     archiveMd = generate_archive_md(searches)
     save_archive_md(archiveMd)
