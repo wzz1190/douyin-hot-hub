0eb02aec1973936b5779483b199caf1380a1af15
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2022-06-24 11:19:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,12 +62,11 @@ def redmd()
     now = util.current_time()
     files = os.path.join('raw', now)
     g = os.walk(files)
-    
-  readme = ''
- readmes = ''
-for path,dir_list,file_list in g:  
+    readme = ''
+    readmes = ''
+    for path,dir_list,file_list in g:  
     for file_name in file_list:  
-      file=(os.path.join(path, file_name) 
+    file=(os.path.join(path, file_name) 
             with open(file) as f:
             readme = f.read()
             readmes=readmes+readme
