74ef80fa3476303029d241d726c1ddf73feaf373
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2022-06-24 11:34:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def redmd():
     for path,dir_list,file_list in g:  
           for file_name in file_list: 
               file=(os.path.join(path, file_name) 
-                 with open(file) as f:
-                     readme = f.read()
+              with open(file) as f:
+                  readme = f.read()
                readmes=readmes+readme
                filee = os.path.join('archives', 'guagua.md')
                util.write_text(filee, readmes)
